81a5e0737c0f8de89c79c011b6a06886365ea010
JuaniVega <juanivega0206@gmail.com>
2024-01-09 15:26:42 -0300

    refactor: update GridPw
    Add the getGridRows method. Apply formatting to the code.

--- src/main/java/in/virit/mopo/GridPw.java
+++ src/main/java/in/virit/mopo/GridPw.java
@@ -2,6 +2,8 @@ package in.virit.mopo;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A helper class to work with the vaadin-grid component.
@@ -13,7 +15,8 @@ public class GridPw {
     /**
      * Creates a Grid page object for the given grid locator.
      *
-     * @param gridLocator the Playwright locator for the grid
+     * @param gridLocator
+     *            the Playwright locator for the grid
      */
     public GridPw(Locator gridLocator) {
         this.root = gridLocator;
@@ -22,7 +25,8 @@ public class GridPw {
     /**
      * Creates a Grid page object for the first grid on the page.
      *
-     * @param page the Playwright page
+     * @param page
+     *            the Playwright page
      */
     public GridPw(Page page) {
         this(page.locator("vaadin-grid"));
@@ -34,8 +38,22 @@ public class GridPw {
      * @return the number of rows
      */
     public int getRenderedRowCount() {
-        Integer evaluate = (Integer) root.elementHandle().evaluate("e => e._getRenderedRows().length");
+        Integer evaluate = (Integer) root.elementHandle()
+                .evaluate("e => e._getRenderedRows().length");
         return evaluate;
+    }
+
+    /**
+     * Returns a list of RowPw objects representing the grid rows.
+     *
+     * @return A List of RowPw objects representing the grid rows.
+     */
+    public List<RowPw> getGridRows() {
+        List<RowPw> allRows = new ArrayList<>();
+        for (int i = 0; i < getRenderedRowCount(); i++) {
+            allRows.add(getRow(i));
+        }
+        return allRows;
     }
 
     /**
@@ -44,68 +62,71 @@ public class GridPw {
      * @return the index.
      */
     public int getFirstVisibleRowIndex() {
-        return (Integer) root.elementHandle().evaluate("e => e._firstVisibleIndex");
+        return (Integer) root.elementHandle()
+                .evaluate("e => e._firstVisibleIndex");
     }
 
     /**
      * Scrolls the grid to the given index.
      *
-     * @param index the row index to scroll to
+     * @param index
+     *            the row index to scroll to
      */
     public void scrollToIndex(int index) {
         root.elementHandle().evaluate("e => e.scrollToIndex(" + index + ")");
 
         // FIXME this don't seem to be stable, but
         // 10*100ms timeout seems to do the trick most of the time
-        int value = (Integer) root.evaluate("""
-                g => {
-                    return new Promise((resolve, reject) => {
-                        var x = 0;
-                        const isDoneLoading = () => {
-                            return !g.$connector.hasRootRequestQueue()
-                        };
-                        
-                        const isVaadinConnectionActive = () => {
-                            if (window.Vaadin && window.Vaadin.Flow && window.Vaadin.Flow.clients) {
-                              var clients = window.Vaadin.Flow.clients;
-                              for (var client in clients) {
-                                if (clients[client].isActive()) {
-                                  return false;
+        int value = (Integer) root.evaluate(
+                """
+                        g => {
+                            return new Promise((resolve, reject) => {
+                                var x = 0;
+                                const isDoneLoading = () => {
+                                    return !g.$connector.hasRootRequestQueue()
+                                };
+
+                                const isVaadinConnectionActive = () => {
+                                    if (window.Vaadin && window.Vaadin.Flow && window.Vaadin.Flow.clients) {
+                                      var clients = window.Vaadin.Flow.clients;
+                                      for (var client in clients) {
+                                        if (clients[client].isActive()) {
+                                          return false;
+                                        }
+                                      }
+                                      return true;
+                                    } else if (window.Vaadin && window.Vaadin.Flow && window.Vaadin.Flow.devServerIsNotLoaded) {
+                                      return false;
+                                    } else {
+                                      return true;
+                                    }
+                                };
+
+                                if (isDoneLoading() && !isVaadinConnectionActive()) {
+                                    resolve(x);
+                                    return;
                                 }
-                              }
-                              return true;
-                            } else if (window.Vaadin && window.Vaadin.Flow && window.Vaadin.Flow.devServerIsNotLoaded) {
-                              return false;
-                            } else {
-                              return true;
-                            }
-                        };
-                        
-                        if (isDoneLoading() && !isVaadinConnectionActive()) {
-                            resolve(x);
-                            return;
-                        }
-                        
-                        var intervalID = window.setInterval(function () {
-                            if (isDoneLoading() && !isVaadinConnectionActive()) {
-                                window.clearInterval(intervalID);
-                                resolve(x+1);
-                            } else {
-                               if (++x === 10) {
-                                   window.clearInterval(intervalID);
-                                   resolve(-1);
-                               }
-                           }
-                        }, 100);
-                    });
-                }""");
-        // System.out.println("RETURN value = " + value);
+
+                                var intervalID = window.setInterval(function () {
+                                    if (isDoneLoading() && !isVaadinConnectionActive()) {
+                                        window.clearInterval(intervalID);
+                                        resolve(x+1);
+                                    } else {
+                                       if (++x === 10) {
+                                           window.clearInterval(intervalID);
+                                           resolve(-1);
+                                       }
+                                   }
+                                }, 100);
+                            });
+                        }""");
     }
 
     /**
      * Selects the given row.
      *
-     * @param rowIndex the row index to select
+     * @param rowIndex
+     *            the row index to select
      */
     public void selectRow(int rowIndex) {
         String script = """
@@ -117,16 +138,18 @@ public class GridPw {
                     var row = rows[0];
                     grid.activeItem = row._item;
                 }
-                """.formatted(rowIndex);
+                """
+                .formatted(rowIndex);
         root.elementHandle().evaluate(script);
     }
 
     /**
      * Returns a RowPw helper representing the row defined by the given index.
      *
-     * @param rowIndex the row index
+     * @param rowIndex
+     *            the row index
      * @return the RowPw for editing the UI state or to get cell locators for
-     * assertions.
+     *         assertions.
      */
     public RowPw getRow(int rowIndex) {
         if (!isRowInView(rowIndex)) {
@@ -138,9 +161,10 @@ public class GridPw {
     /**
      * Checks if the given row is in the visible viewport.
      *
-     * @param rowIndex the row to check
+     * @param rowIndex
+     *            the row to check
      * @return <code>true</code> if the row is at least partially in view,
-     * <code>false</code> otherwise
+     *         <code>false</code> otherwise
      */
     public boolean isRowInView(int rowIndex) {
         return (getFirstVisibleRowIndex() <= rowIndex
@@ -153,7 +177,8 @@ public class GridPw {
      * @return the index
      */
     public int getLastVisibleRowIndex() {
-        return (Integer) root.elementHandle().evaluate("e => e._lastVisibleIndex");
+        return (Integer) root.elementHandle()
+                .evaluate("e => e._lastVisibleIndex");
     }
 
     /**
@@ -172,23 +197,30 @@ public class GridPw {
         /**
          * Gets the cell locator at the given index.
          *
-         * @param cellIndex the cell index (0-based, unlike the CSS nth-child
-         * selector, whose designer should be hung by the balls, in case they
-         * have any)
+         * @param cellIndex
+         *            the cell index (0-based, unlike the CSS nth-child
+         *            selector, whose designer should be hung by the balls, in
+         *            case they have any)
          * @return the cell locator
          */
         public Locator getCell(int cellIndex) {
-            int indexInVirtualTable = (Integer) root.evaluate("g => g._getRenderedRows().indexOf(g._getRenderedRows().filter(r => r.index == %s)[0]);".formatted(rowIndex));
+            int indexInVirtualTable = (Integer) root.evaluate(
+                    "g => g._getRenderedRows().indexOf(g._getRenderedRows().filter(r => r.index == %s)[0]);"
+                            .formatted(rowIndex));
             indexInVirtualTable += 1; // 1-based :-)
-            String name = root.locator("#items tr:nth-child(%s) td:nth-child(%s) slot".formatted(indexInVirtualTable, cellIndex + 1))
+            String name = root
+                    .locator("#items tr:nth-child(%s) td:nth-child(%s) slot"
+                            .formatted(indexInVirtualTable, cellIndex + 1))
                     .getAttribute("name");
-            return root.locator("vaadin-grid-cell-content[slot='%s']".formatted(name));
+            return root.locator(
+                    "vaadin-grid-cell-content[slot='%s']".formatted(name));
         }
 
         /**
          * Gets the cell with the given header text.
          *
-         * @param headerText the header text
+         * @param headerText
+         *            the header text
          * @return the cell locator
          */
         public Locator getCell(String headerText) {
